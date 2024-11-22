1e02742935e9c434c1b16335b8b370af5501c490
jacob moya <jemoya1994@Gmail.com>
2024-11-21 22:02:06 -0800

    updated benifits/ misc sections, nothing to complicated, simple mapping over a arrya of strings for a li

--- src/Components/KeyItems.tsx
+++ src/Components/KeyItems.tsx
@@ -3,10 +3,10 @@ import { FaArtstation, FaDownload, FaThumbsUp } from 'react-icons/fa6'
 
 const KeyItems = () => {
   return (
-    <div className='shadow-xl w-10/12 md:w-2/3 p-8 rounded-lg bg-zinc-100 -my-16 flex items-center justify-center mx-auto gap-16 relative z-10'   >
-        <Header2 h2Class={'text-slate-800 flex items-center gap-4 text-center flex-col'} text={'Simple to Customize'} innerIcon={<FaThumbsUp/>}/>
-        <Header2 h2Class={'text-slate-800 flex items-center gap-4 text-center flex-col'} text={'Add Personal Logos'} innerIcon={<FaArtstation/>}/>
-        <Header2 h2Class={'text-slate-800 flex items-center gap-4 text-center flex-col'} text={'High Quality Donwloads'} innerIcon={<FaDownload/>}/>
+    <div className='shadow-xl w-10/12 md:w-2/3 p-8 rounded-lg flex-wrap bg-zinc-100 -my-16 flex items-center justify-center mx-auto relative z-10'   >
+        <Header2 h2Class={'text-slate-800 flex items-center gap-4 text-center flex-col w-full md:w-4/12'} text={'Simple to Customize'} innerIcon={<FaThumbsUp/>}/>
+        <Header2 h2Class={'text-slate-800 flex items-center gap-4 text-center flex-col w-full md:w-4/12 my-8 md:my-0'} text={'Add Personal Logos'} innerIcon={<FaArtstation/>}/>
+        <Header2 h2Class={'text-slate-800 flex items-center gap-4 text-center flex-col w-full md:w-4/12'} text={'High Quality Donwloads'} innerIcon={<FaDownload/>}/>
     </div>
   )
 }
--- src/Layout/OurBenifits.tsx
+++ src/Layout/OurBenifits.tsx
@@ -1,20 +1,37 @@
+import {  FaSquareCheck } from "react-icons/fa6"
 import Header2 from "../Components/Header2"
 import JakeCharacter from "../assets/JAKE-CARTOON.png"
  
+
+
 const OurBenifits = ( ) => {
-  return (
-        <div className=" bg-white text-center md:text-left  w-full flex flex-wrap mx-auto min-h-40   p-8 md:p-16 md:pt-0">
-            <div className="rounded-md md:w-5/12 w-full p-8 flex items-center justify-center bg-slate-100 ">
-                <img src={JakeCharacter} alt="jacob cartoon version "/>
-            </div>
-            <div className=" md:w-7/12 w-full bg-white p-8 px-16">
-                <Header2 h2Class={'text-2xl mb-4'} text={"Benefits of JakeQR"} innerIcon={undefined}/>
+    
+    const listItems:string[] = [
+        'Fun Application',
+        'Easy To Use',
+        'Fast Downloads',
+        'High Quality PNGs',
+        'SVGs coming Soon!'
+    ]
 
-                By using my fun litte App, you can create fully customized QR Codes w/ Logo to use with your respective link. Please note, this does need to be a live weblink. More features to be added soon!
+    return (
+            <div className=" bg-white text-center md:text-left  w-full flex flex-wrap mx-auto min-h-40   p-8 md:p-16 md:pt-0">
+                <div className="rounded-md md:w-5/12 w-full p-8 flex items-center justify-center bg-yellow-500 ">
+                    <img src={JakeCharacter} alt="jacob cartoon version "/>
+                </div>
+                <div className="md:mt-0 mt-16 md:w-7/12 w-full flex justify-between flex-col bg-white md:px-16">
+                    <Header2 h2Class={'text-3xl '} text={"Benefits of JakeQR"} innerIcon={undefined}/>
 
-            </div>
-        </div>    
-    )
+                    <p className="mt-6 md:mb-8 mb-16">By using my fun litte App, you can create  customized QR Codes w/ Logo to use with your respective link. Please note, this does need to be a live weblink. <br/><br/> <b className="text-lg">More features to be added soon!</b></p>
+
+                <ul className="md:columns-2 bg-slate-900 rounded-lg text-white p-8">
+                        {listItems.map((item)=>(
+                            <li className=" md:text-xl flex items-center gap-4 py-2" id={item}> <FaSquareCheck className="text-yellow-500"/> {item} </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>    
+        )
 }
 
 export default OurBenifits
